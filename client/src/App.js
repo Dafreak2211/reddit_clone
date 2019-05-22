ef7b93f392abd1826258e97ca7776dbfb7363b63
haitran <sl4mdunk1999@gmail.com>
2019-05-22 18:50:22 +0700

    setup react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={() => <Home />} />
+          <Route exact path="/register" component={() => <Register />} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
